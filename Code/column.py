7c66b27735548bab2aaefeb15ee1a5a3fb18f3a1
ronakkaoshik42 <49129684+ronakkaoshik42@users.noreply.github.com>
2019-11-03 01:10:49 +0530

    Add files via upload

---
+++ Code/column.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import spacy
+from spacy import displacy
+from difflib import get_close_matches
+def give_column(sen_without_ne,column_names):#column_names=['Place','Affiliate','AffiliationID','AuthID','FieldID','Name','ConfID','PaperID','Venue','Year','Topic','KeywordID','Summary','Title']
+    lower_column_names=[]
+    for i in column_names:
+        lower_column_names.append(i.lower())
+    nlp = spacy.load("en_core_web_sm")
+    doc=nlp(sen_without_ne)
+    l=[token.text for token in doc if (token.pos_ == "NOUN" or token.pos_=="PROPN")]
+    a=[]
+    x=[]
+    for i in l:
+        x=get_close_matches(i.lower(), lower_column_names,n=1,cutoff=0.46)
+        if not x==[]:
+            for j in range(len(lower_column_names)):
+                if lower_column_names[j]==x[0]:
+                    a.append(column_names[j])
+                    break
+                else:
+                    continue
+        else:
+            print('error')
+    return a
